b21252ca4e357bfec7907c9238a3c0ede7d14520
Jon Holtzman <holtz@nmsu.edu>
2016-07-29 13:54:09 -0600

    add comments

--- plots.py
+++ plots.py
@@ -37,7 +37,7 @@ def event(fig) :
             struct.list(_data,ind=_index,cols=_id_cols)
     cid = fig.canvas.mpl_connect('key_press_event',onpress)
 
-def plotc(ax,x,y,z,xr=None,yr=None,zr=None,size=5,cmap='rainbow',colorbar=False,xt=None,yt=None,zt=None,label=None,linewidth='0',marker='o',draw=True,orientation='vertical') :
+def plotc(ax,x,y,z,xr=None,yr=None,zr=None,size=5,cmap='rainbow',colorbar=False,xt=None,yt=None,zt=None,label=None,linewidth='0',marker='o',draw=True,orientation='vertical',labelcolor='k') :
     """
     Plots a scatter plot with point color-coded by z data
 
@@ -48,17 +48,23 @@ def plotc(ax,x,y,z,xr=None,yr=None,zr=None,size=5,cmap='rainbow',colorbar=False,
       z (float)  : z values, or a single color
 
     Keyword args:
-      xr : x range (optional)
-      yr : y range (optional)
-      zr : z range (optional)
-      xt : x axis title (optional)
-      yt : y axis title (optional)
-      zt : z axis title (optional)
-      marker : marker type
-      cmap : colormap
-      size : point size(s) in points (optional)
+      xr : x range  (default=None)
+      yr : y range (default=None)
+      zr : z range (default=None)
+      xt : x axis title (default=None) (default=None)
+      yt : y axis title (default=None)
+      zt : z axis title (default=None)
+      marker : marker type (default='o')
+      cmap : colormap (default='rainbow')
+      size : point size(s) in points (default=5)
+      linewidth : linewidth (default='0')
+      colorbar (bool) : draw a colorbar? (default=False)
+      orientation : colorbar orientation (default='vertical')
+      label [x,y,text] : label plot with text at position x,y in relative (0:1,0:1) coordinates
+      labelcolor : color for label
 
     Returns:
+      aximage
 
     """
     if xr is not None : ax.set_xlim(xr[0],xr[1])
@@ -71,7 +77,7 @@ def plotc(ax,x,y,z,xr=None,yr=None,zr=None,size=5,cmap='rainbow',colorbar=False,
     else :
         scat=ax.scatter(x,y,c=z,vmin=zr[0],vmax=zr[1],s=size,cmap=cmap,linewidth=linewidth,marker=marker)
     if label is not None :
-        ax.text(label[0],label[1],label[2],transform=ax.transAxes) 
+        ax.text(label[0],label[1],label[2],transform=ax.transAxes,color=labelcolor) 
     if colorbar :
         cb=plt.colorbar(scat,ax=ax,orientation=orientation)
         cb.ax.set_ylabel(zt)
@@ -110,9 +116,29 @@ def plotrow(ax,img,r,norm=True,draw=True) :
             ax.plotl(np.sum(img[r[0]:r[1],:],axis=1))
     if draw : plt.draw()
 
-def plotp(ax,x,y,z=None,typeref=None,types=None,xr=None,yr=None,zr=None,marker='o',size=5,linewidth='0',color='r',facecolors=None,xt=None,yt=None,draw=True,xerr=None,yerr=None,label=None) :
+def plotp(ax,x,y,z=None,typeref=None,types=None,xr=None,yr=None,zr=None,marker='o',size=5,linewidth='0',color='r',facecolors=None,xt=None,yt=None,draw=True,xerr=None,yerr=None,label=None,labelcolor='k') :
     '''
     Plot points, optionally with a series of different markers/sizes keyed to z data
+
+    Args:
+        ax : axes to plot in
+        x : x data
+        y : y data
+
+    Keyword args:
+        z=  : specifies data to be used to color points if using types to specify sizes, markers (default=None)
+        typeref= : specfies array to be used to determine groupings
+        types= : array of different types (from typeref) to plot with specified sizes, markers, colors
+        size= : array of different sizes to plot for different types, or single size
+        marker= : array of different markers to plot for different types, or single marker
+        color= : array of different colors markers to plot for different types, or single color
+        xr : x limits (default=None)
+        yr : y limits (default=None)
+        xt : x title (default=None)
+        yt : y title (default=None)
+        label=[x,y,text] : put text at (x,y) relative coords
+        labelcolor=  : color for label
+       
     '''
     if xr is not None : ax.set_xlim(xr[0],xr[1])
     if yr is not None : ax.set_ylim(yr[0],yr[1])
@@ -159,7 +185,8 @@ def plotp(ax,x,y,z=None,typeref=None,types=None,xr=None,yr=None,zr=None,marker='
             ax.errorbar(x,y,marker=marker,xerr=xerr,yerr=yerr,fmt='none',capsize=0,ecolor=color)
 
     if label is not None :
-        ax.text(label[0],label[1],label[2],transform=ax.transAxes)
+        if labelcolor is 'k' and color is not None : labelcolor=color
+        ax.text(label[0],label[1],label[2],transform=ax.transAxes,color=labelcolor)
 
     if draw : plt.draw()
 

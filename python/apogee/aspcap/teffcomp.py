5899c2c50173c8c7f899870804ee5d391d5aff7c
Jon Holtzman <holtz@nmsu.edu>
2018-06-27 04:25:11 -0600

    add dr13 consistency attempt, ghb routine

--- python/apogee/aspcap/teffcomp.py
+++ python/apogee/aspcap/teffcomp.py
@@ -4,13 +4,11 @@ from apogee.utils import apload
 from apogee.utils import apselect
 from astropy.io import fits
 from astropy.io import ascii
-#from holtz.gal import isochrones
-#from holtz.gal import stars
 from tools import match
 from tools import plots
 from tools import fit
 from apogee.utils import bitmask
-from apogee.aspcap import cal
+from apogee.aspcap import err
 import pdb
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,7 +27,7 @@ def bindata(xdata,ydata,bins,median=True) :
           mean[i]=ydata[j].mean() 
     return mean
 
-def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[-2.5,0.75],alpha=False,out='teffcomp',yr=[-500,500],calib=False) :
+def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[-2.5,0.75],alpha=False,out='teffcomp',yr=[-500,500],calib=False,dr13=False) :
     """
     Compares allstar ASPCPAP Teff with photometric Teff from GHB for sample of stars with GLAT>glatmin and SFD_EBV<ebvmax,
     does fits
@@ -47,18 +45,25 @@ def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[
     if dwarf :
         gd=apselect.select(allstar,badval=['STAR_BAD'],badtarg=['EMBEDDED','EXTENDED'],teff=trange,mh=mhrange,logg=[3.8,5.0],raw=True)
     else :
-        gd=apselect.select(allstar,badval=['STAR_BAD'],badtarg=['EMBEDDED','EXTENDED'],teff=trange,mh=mhrange,logg=[0,3.8],raw=True)
+        if dr13:
+          gd=apselect.select(allstar,badval=['STAR_BAD'],teff=trange,mh=mhrange,logg=[0,3.8],raw=True)
+        else :
+          gd=apselect.select(allstar,badval=['STAR_BAD'],badtarg=['EMBEDDED','EXTENDED'],teff=trange,mh=mhrange,logg=[0,3.8],raw=True)
     allstar=allstar[gd]
-    j=np.where((abs(allstar['GLAT'])>glatmin)&(allstar['SFD_EBV']>-0.01)&(allstar['SFD_EBV']<ebvmax)&(abs(allstar['J'])<90)&(abs(allstar['K'])<90))[0]
+    if dr13 :
+      j=np.where((abs(allstar['GLAT'])>glatmin)&(allstar['SFD_EBV']<ebvmax))[0]
+    else :
+      j=np.where((abs(allstar['GLAT'])>glatmin)&(allstar['SFD_EBV']>-0.01)&(allstar['SFD_EBV']<ebvmax)&(abs(allstar['J'])<90)&(abs(allstar['K'])<90))[0]
     if calib : 
         param='PARAM'
     else :
         param='FPARAM'
 
     # remove second gen GC stars
-    gcstars = ascii.read(os.environ['IDLWRAP_DIR']+'/data/gc_szabolcs.dat')
-    bd=np.where(gcstars['pop'] != 1)[0]
-    j = [x for x in j if allstar[x]['APOGEE_ID'] not in gcstars['id'][bd]]
+    if not dr13 :
+      gcstars = ascii.read(os.environ['APOGEE_DIR']+'/data/calib/gc_szabolcs.dat')
+      bd=np.where(gcstars['pop'] != 1)[0]
+      j = [x for x in j if allstar[x]['APOGEE_ID'] not in gcstars['id'][bd]]
 
     allstar=allstar[j]
 
@@ -66,19 +71,22 @@ def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[
     fig,ax=plots.multi(1,1,hspace=0.001,wspace=0.001,figsize=(12,6))
     xr=[-3.0,1.0]
     zr=trange
+    if dr13: zr=[3500,5500]
     binsize=0.25
     bins=np.arange(-2.5,0.75,binsize)
     # diff color-coded by gravity as f([M/H])
-    ghb,dtdjk=stars.ghb(allstar['J']-allstar['K'],allstar[param][:,3],dwarf=dwarf)
-    gd=np.where(abs(allstar[param][:,0]-ghb) < 500)[0]
-    ghb=ghb[gd]
-    dtdjk=dtdjk[gd]
-    allstar=allstar[gd]
+    ghb,dtdjk=cte_ghb(allstar['J']-allstar['K'],allstar[param][:,3],dwarf=dwarf)
+    if not dr13 :
+      gd=np.where(abs(allstar[param][:,0]-ghb) < 500)[0]
+      ghb=ghb[gd]
+      dtdjk=dtdjk[gd]
+      allstar=allstar[gd]
+    print('number of stars: ', len(allstar))
 
     if alpha :
-        plots.plotc(ax,allstar[param][:,3],allstar[param][:,0]-ghb,allstar[param][:,6],zr=[-0.1,0.4],xr=xr,yr=yr,xt='[M/H]',yt='ASPCAP-photometric Teff',colorbar=True,zt=r'[$\alpha$/M]')
+        plots.plotc(ax,allstar[param][:,3],allstar[param][:,0]-ghb,allstar[param][:,6],zr=[-0.1,0.4],xr=xr,yr=yr,xt='[M/H]',yt='ASPCAP-photometric Teff',colorbar=True,zt=r'[$\alpha$/M]',rasterized=True)
     else :
-        plots.plotc(ax,allstar[param][:,3],allstar[param][:,0]-ghb,allstar[param][:,0],zr=zr,xr=xr,yr=yr,xt='[M/H]',yt='ASPCAP-photometric Teff',colorbar=True,zt='$T_{eff}$')
+        plots.plotc(ax,allstar[param][:,3],allstar[param][:,0]-ghb,allstar[param][:,0],zr=zr,xr=xr,yr=yr,xt='[M/H]',yt='ASPCAP-photometric Teff',colorbar=True,zt='$T_{eff}$',rasterized=True)
     mean=bindata(allstar[param][:,3],allstar[param][:,0]-ghb,bins,median=False)
     plots.plotp(ax,bins+binsize/2.,mean,marker='o',size=40)
     mean=bindata(allstar[param][:,3],allstar[param][:,0]-ghb,bins,median=True)
@@ -88,14 +96,17 @@ def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[
     # 1D quadratic fit as a function of metallicity
     allfit = fit.fit1d(allstar[param][:,3],allstar[param][:,0]-ghb,ydata=allstar[param][:,0],degree=2,reject=0)
     ejk=np.clip(np.sqrt(allstar['J_ERR']**2+allstar['K_ERR']**2),0.,0.02)
-    #errpar = cal.errfit(allstar[param][:,0],allstar['SNR'],allstar[param][:,3],allstar[param][:,0]-tefit(allstar[param][:,3])-ghb,title='Teff',out=out+'_phot',zr=[0,250],meanerr=abs(dtdjk)*ejk)
-    errpar = cal.errfit(allstar[param][:,0],allstar['SNR'],allstar[param][:,3],allstar[param][:,0]-tefit(allstar[param][:,3])-ghb,title='Teff',out=out,zr=[0,150])
+    #errpar = err.errfit(allstar[param][:,0],allstar['SNR'],allstar[param][:,3],allstar[param][:,0]-tefit(allstar[param][:,3])-ghb,title='Teff',out=out+'_phot',zr=[0,250],meanerr=abs(dtdjk)*ejk)
+    errpar = err.errfit(allstar[param][:,0],allstar['SNR'],allstar[param][:,3],allstar[param][:,0]-tefit(allstar[param][:,3])-ghb,title='Teff',out=out,zr=[0,150])
 
     rms = (allstar[param][:,0]-tefit(allstar[param][:,3])-ghb).std()
     ax.text(0.98,0.9,'rms: {:6.1f}'.format(rms),transform=ax.transAxes,ha='right')
     x=np.linspace(-3,1,200)
     plots.plotl(ax,x,tefit(x),color='k')
-    #plots.plotl(ax,x,allfit(x),color='b')
+    if dr13: 
+      plots.plotl(ax,x,-36.17+95.97*x-15.09*x**2,color='g')
+      plots.plotl(ax,x,allfit(x),color='b')
+      print(allfit)
     plots._data_x = allstar[param][:,3]
     plots._data_y = allstar[param][:,0]-ghb
     plots._data = allstar
@@ -119,6 +130,9 @@ def ghb(allstar,glatmin=30.,ebvmax=0.03,dwarf=False,trange=[3750,5500],mhrange=[
 
     fig.tight_layout()
     fig.savefig(out+'.jpg')
+    plt.rc('font',size=14)
+    plt.rc('axes',titlesize=14)
+    plt.rc('axes',labelsize=14)
     fig.savefig(out+'.pdf')
 
     # auxiliary plots with different color-codings
@@ -162,7 +176,7 @@ def irfm(allstar,trange=[4000,5000],mhrange=[-2.5,0.75],out='dteff') :
     allstar=allstar[gd]
 
     # get IRFM data
-    irfm=fits.open(os.environ['IDLWRAP_DIR']+'/data/irfm_temp.fits')[1].data
+    irfm=fits.open(os.environ['APOGEE_DIR']+'/data/calib/irfm_temp.fits')[1].data
 
     # get the subsamples and match. Note that we have to do this separately for each subsample because some
     #   stars appear in more than one subsample
@@ -272,4 +286,18 @@ def dr13dr12() :
     plots.plotc(ax[0],dr13['M_H'],dr13['TEFF']-dr12['TEFF'],dr13['TEFF'],xr=[-2.5,0.75],yr=[-300,300],zr=[3500,5000])
 
     plots.plotc(ax[1],dr13['TEFF'],dr13['TEFF']-dr12['TEFF'],dr13['M_H'],xr=[6500,3000],yr=[-300,300],zr=[-2,0.5])
+
+def cte_ghb(jk0,feh,dwarf=False) :
+    """
+    Color-temperature relation from Gonzalez Hernandez & Bonifacio (2009):  (J-K)_0 - Teff
+    """
+    if dwarf :
+      b0=0.6524 ; b1=0.5813 ; b2=0.1225 ; b3=-0.0646 ; b4=0.0370 ; b5=0.0016 # dwarfs
+    else :
+      b0=0.6517 ; b1=0.6312 ; b2=0.0168 ; b3=-0.0381 ; b4=0.0256 ; b5=0.0013 # giants
+    theta=b0+b1*jk0+b2*jk0**2+b3*jk0*feh+b4*feh+b5*feh**2
+    dtheta_djk = b1+2*b2*jk0+b3*feh
+    dt_djk= -5040./theta**2*dtheta_djk
+
+    return 5040./theta, dt_djk
 

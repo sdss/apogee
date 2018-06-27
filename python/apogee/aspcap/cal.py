7eb48401c163bcaf5c3adf6d69b0069fab96b04d
Jon Holtzman <holtz@nmsu.edu>
2018-06-27 04:24:22 -0600

    separate err routines to avoid circular dependency

--- python/apogee/aspcap/cal.py
+++ python/apogee/aspcap/cal.py
@@ -1,11 +1,14 @@
 from apogee.utils import apload
 from apogee.utils import apselect
 from apogee.aspcap import elem
+from apogee.aspcap import teffcomp
+from apogee.aspcap import loggcomp
 from tools import html
 from tools import match
 from tools import struct
 from tools import plots
 from tools import fit
+from tools import vfit
 import os
 import shutil
 import pdb
@@ -264,9 +267,6 @@ def docal(vers,clobber=False,allstar=True,hr=True,teff=True,logg=True,vmicro=Tru
 
     allcal={}
     # Teff vs photometric
-    from holtz.apogee import teffcomp
-    reload(teffcomp)
-    from holtz.tools import fit
     if teff :
         allcal['giant_teffcal'] = teffcomp.ghb(c,ebvmax=0.02,glatmin=10,out=out+'giant_teffcomp',yr=[-750,750],dwarf=False,calib=calib)
         if out is not None :
@@ -281,8 +281,6 @@ def docal(vers,clobber=False,allstar=True,hr=True,teff=True,logg=True,vmicro=Tru
     ytitle.append('Teff')
 
     # log g vs asteroseismic
-    from holtz.apogee import loggcomp
-    reload(loggcomp)
     if logg :
         allcal['rgbrcsep' ] = loggcomp.rcrgb(c,out=out+'rcrgbsep')
         allcal['loggcal'] = loggcomp.apokasc(c,plotcal=False,out=out+'loggcomp',calib=calib)
@@ -295,10 +293,6 @@ def docal(vers,clobber=False,allstar=True,hr=True,teff=True,logg=True,vmicro=Tru
     ytitle.append('log g')
 
     # vmicro calibration
-    from holtz.apogee import vfit
-    reload(vfit)
-    from holtz.tools import fit
-    reload(fit)
     if vmicro :
         print("vmicro fit, cubic in log g, linear in [M/H]")
         print("sample limited to FERRE vmicro error <0.01 ")
@@ -321,8 +315,6 @@ def docal(vers,clobber=False,allstar=True,hr=True,teff=True,logg=True,vmicro=Tru
 
     # elemental abundances
     if elemcal :
-        from holtz.apogee import elem
-        reload(elem)
         elems=np.append(cc['ELEM_SYMBOL'][0],['M','alpha'])
         # use allCal file for uncertainty calibration, so we have multiple visits
         # use allstar for calibration, so we have full solar circle sample
@@ -462,98 +454,4 @@ def allplots() :
     os.chdir('../dr12')
     apload.dr12()
     errplots(tags=['PARAM_ALPHA_M','O_H','MG_H','NI_H','PARAM_M_H'])
-
-def errfit(te, snr, mh, val, snbins=np.arange(50,250,50), tebins=np.arange(3500,6000,250), mhbins=np.arange(-2.25,0.75,0.5),verbose=False,out=None,title='', zr=[0,0.1], snplot=True, meanerr=None ) :
-    '''
-    Fits for empirical uncertainty as function of Teff, S/N, and [M/H]
-    '''
-    if out is not None :
-        fig,ax=plots.multi(len(snbins),1,wspace=0.001,figsize=(3*len(snbins),2))
-
-    # bin sizes and initialize data arrays
-    dte = tebins[1]-tebins[0]
-    dmh = mhbins[1]-mhbins[0]
-    dsn = snbins[1]-snbins[0]
-    rmsdata=[]
-    rmsderiv=[]
-    nbin=[]
-    # accumulate the data: rms in bins of Teff, S/N, and [M/H]
-    snmin=snbins[-1]
-    snmax=snbins[0]
-    temin=tebins[-1]
-    temax=tebins[0]
-    mhmin=mhbins[-1]
-    mhmax=mhbins[0]
-    for mhbin in mhbins :
-        for tebin in tebins :
-            for snbin in snbins :
-                ibin = np.where(( te > tebin) & (te <= tebin+dte) &
-                                ( mh > mhbin ) & (mh <= mhbin+dmh) &
-                                ( snr > snbin) & (snr <= snbin+dsn) & (val > -9990.) )[0]
-                if len(ibin) > 3 :
-                    if meanerr is not None :
-                        err = np.sqrt(np.clip(val[ibin].std()**2 - np.median(meanerr[ibin])**2,0.001,10000000.))
-                    else :
-                        err = val[ibin].std()
-                    rmsdata.append(err)
-                    rmsderiv.append([1.,tebin+dte/2.-4500.,snbin+dsn/2.-100.,mhbin+dmh/2.])
-                    if verbose :
-                        print(tebin+dte/2.,snbin+dsn/2.,mhbin+dmh/2.,err,len(ibin))
-                    snmin=np.array([snmin,snbin]).min()
-                    snmax=np.array([snmax,snbin]).max()
-                    temin=np.array([temin,tebin]).min()
-                    temax=np.array([temax,tebin]).max()
-                    mhmin=np.array([mhmin,mhbin]).min()
-                    mhmax=np.array([mhmax,mhbin]).max()
-                    if out is not None :
-                        iplt=np.where(snbins == snbin)[0][0]
-                        plots.plotc(ax[iplt],mhbin+dmh/2.,tebin+dte/2.,err,xr=[mhbins[0],mhbins[-1]],yr=[tebins[0],tebins[-1]],zr=zr,size=30,linewidth=1)
-
-    # do the fit in log(rms) so that empirical uncertainty is positive-definite
-    rmsdata=np.log(np.array(rmsdata))
-    rmsderiv=np.array(rmsderiv)
-    if len(rmsdata) > 5 :
-        soln,inv = fit.linear(rmsdata,rmsderiv.transpose())
-
-    figs=[]
-    if out is not None :
-        y, x = np.mgrid[tebins[0]:tebins[-1]:200j,mhbins[0]:mhbins[-1]:200j]
-        for iplt in range(len(snbins)) :
-            sn = snbins[iplt]+dsn/2.
-            ax[iplt].imshow(elemerr(soln,y-4500.,sn-100.,x),extent=[mhbins[0],mhbins[-1],tebins[0],tebins[-1]], aspect='auto',vmin=zr[0],vmax=zr[1], origin='lower',cmap='rainbow')
-            ax[iplt].text(0.98,0.98,title+' S/N={:4.0f}'.format(sn),va='top',ha='right',transform=ax[iplt].transAxes)
-
-        fig.savefig(out+'_err.jpg')
-        figs.append([os.path.basename(out+'_err.jpg')])
-
-        if snplot :
-            fig,ax=plots.multi(len(tebins),len(mhbins),wspace=0.001,hspace=0.001,figsize=(2*len(tebins),2*len(mhbins)))
-            for ix in range(len(tebins)) :
-              if ix == 0 : yt=r'$\sigma$'
-              else : yt=''
-              for iy in range(len(mhbins)) :
-                plt=np.where((np.isclose(rmsderiv[:,1]+4500,tebins[ix]+dte/2.)) & (np.isclose(rmsderiv[:,3],mhbins[iy]+dmh/2.)))[0]
-                plots.plotc(ax[iy,ix],rmsderiv[plt,2]+100,np.exp(rmsdata[plt]),rmsderiv[plt,3],size=30,zr=[-2,0.5],
-                            yr=zr,xr=[snbins[0],snbins[-1]],xt='S/N',yt=yt)
-                ax[iy,ix].text(0.98,0.98,'{:8.0f} {:8.2f}'.format(tebins[ix]+dte/2.,mhbins[iy]+dmh/2.),ha='right',va='top',transform=ax[iy,ix].transAxes)
-            fig.savefig(out+'_err_sn.jpg')
-            figs.append([os.path.basename(out+'_err_sn.jpg')])
-
-        html.htmltab(figs,file=out+'_err.html',header=title+' empirical uncertainties')
-
-    if verbose : 
-        print(soln)
-        print(snmin,snmax,temin,temax,mhmin,mhmax)
-        pdb.set_trace()
-
-    return soln
-
-
-def elemerr(soln,te,sn,fe) :
-    ''' 
-    Function to evaluate fit for uncertainty
-    '''
-    out=soln[0]+soln[1]*te+soln[2]*sn
-    if len(soln) > 3: out+= soln[3]*fe
-    return np.exp(out)
 

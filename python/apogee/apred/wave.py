b81b8e39eb20f1318dc7f33f21215085d716e31d
Jon Holtzman <holtz@nmsu.edu>
2018-11-06 16:58:14 -0700

    updates to wavecal, remove make calibration files routines

--- python/apogee/apred/wave.py
+++ python/apogee/apred/wave.py
@@ -30,6 +30,7 @@ from scipy import interpolate
 from apogee.utils import apload
 from tools import plots
 from sdss import yanny
+from astropy.table import Table
 
 chips=['a','b','c']
 colors=['r','g','b','c','m','y']
@@ -189,7 +190,7 @@ def findlines(frame,rows,waves,lines,out=None,verbose=False,estsig=2) :
                     if verbose : print('failed: ',num,row,chip)
     return linestr[0:nline]
 
-def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose=False) :
+def wavecal(nums=[2420038],name=None,inst='apogee-n',rows=[150],npoly=4,reject=3,plot=False,hard=None,verbose=False,clobber=False,init=False) :
     """ APOGEE wavelength calibration
 
     Solves for wavelength calibration given input frameid(s) allowing for a single polynomial
@@ -202,17 +203,23 @@ def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose
         verbose (bool) :  plot fits (default=False)
     """
     apload.apred='t9'
+    apload.instrument=inst
 
+    if name is None : name = nums[0]
     # Initial guess for wavelengths, used to find lines
     coef0 = {}
     if inst == 'apogee-n' :
         coef0['a'] = np.flip([ 16955.45703, -0.2128979266, -1.117692409e-05])
         coef0['b'] = np.flip([ 16434.20508, -0.2613874376, -1.035568130e-05])
         coef0['c'] = np.flip([ 15809.69238, -0.3065520823, -9.610030247e-06])
+        pars0 = [1.19112154e-10,-1.03229705e-05,-2.82422124e-01,1.61568801e+04,
+                -1.44043125e+02,0.00000000e+00,1.54456264e+02]
     else :
         coef0['a'] = np.flip([  16957.7252,  -2.14859462e-01,  -1.09959211e-05])
         coef0['b'] = np.flip([  16432.4720,  -2.63317139e-01,  -1.03074667e-05])
         coef0['c'] = np.flip([  15802.3346,  -3.08933509e-01,  -9.45618858e-06])
+        pars0 = [ 1.19138048e-10,-1.03101159e-05,-2.84129914e-01,1.61531282e+04,
+                 -1.49566344e+02,-9.99666738e-11, 1.58164526e+02]
     waves = {}
     pixels = np.arange(2048)
     for chip in chips : waves[chip] = np.tile(np.polyval(coef0[chip],pixels),(300,1))
@@ -221,13 +228,21 @@ def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose
     print('finding lines with initial wavelength guess: ',coef0)
     ngroup=1
     for inum,num in enumerate(nums) :
+        # load 1D frame
         frame = apload.ap1D(num)
+        # get correct arclines
         if frame['a'][0].header['LAMPUNE'] : lampfile = 'UNe.vac.apogee'
         if frame['a'][0].header['LAMPTHAR'] : lampfile = 'tharne.lines.vac.apogee'
         arclines=ascii.read(os.environ['APOGEE_DIR']+'/data/arclines/'+lampfile)
         j=np.where(arclines['USEWAVE'])[0]
         arclines=arclines[j]
-        flinestr = findlines(frame,rows,waves,arclines,verbose=verbose,estsig=2)
+        # find lines or use previous found lines
+        linesfile='apLines-{:08d}.fits'.format(num)
+        if os.path.exists(linesfile) and not clobber :
+            flinestr = fits.open(linesfile)[1].data
+        else :
+            flinestr = findlines(frame,rows,waves,arclines,verbose=verbose,estsig=2)
+            Table(flinestr).write(linesfile,overwrite=True)
         # replace frameid tag with group identification, which must start at 0 for func_multi_poly indexing
         if inum > 0 and abs(num-nums[inum-1]) > 1 : ngroup +=1
         flinestr['frameid'] = ngroup-1
@@ -235,24 +250,28 @@ def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose
         else : linestr = np.append(linestr,flinestr)
         print(' Frame: {:d}  Nlines: {:d}  '.format(num,len(flinestr)))
 
-    maxiter=7
-    reject = 3.
-    allpars = []
+    # do the wavecal fit
     # initial parameter guess for first row, subsequent rows will use guess from previous row
     npars=npoly+3*ngroup
     pars = np.zeros(npars)
-    #  initial quadratic relation
-    pars[npoly-3:npoly] = coef0['b']
+    # initial quadratic relation from chip b and initial chip offsets or better guess if we have it
+    if init :pars[npoly-3:npoly] = coef0['b']
+    else : pars[npoly-4:] = pars0
+
+    # set up output arrays for all 300 fibers
     allpars=np.zeros([npars,300])
     rms=np.zeros(300)
     sig=np.zeros(300)
     if plot : 
         fig,ax=plots.multi(1,3,hspace=0.001,wspace=0.001)
-        fig2,ax2=plots.multi(1,1,hspace=0.001,wspace=0.001)
+        fig2,ax2=plots.multi(1,4,hspace=0.001,wspace=0.001)
+    # loop over requested rows
     for irow,row in enumerate(rows) :
+        # position of green chip in first group fixed to 0., otherwise no bounds
         bounds = ( np.zeros(len(pars))-np.inf, np.zeros(len(pars))+np.inf)
         bounds[0][npoly+1] =  -1.e-10
         bounds[1][npoly+1] =  1.e-10
+        # set up independent variable array with pixel, chip, groupid, and dependent variable (wavelength)
         thisrow = np.where(linestr['row'] == row)[0]
         x = np.zeros([3,len(thisrow)])
         x[0,:] = linestr['pixel'][thisrow]
@@ -260,23 +279,47 @@ def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose
         x[2,:] = linestr['frameid'][thisrow]
         y = linestr['wave'][thisrow]
         res = y-func_multi_poly(x,*pars)
+        if irow == 0 : maxiter=15
+        else : maxiter=7
         for niter in range(maxiter) :
+            # reject lines that have bad residuals
             gd = np.where(abs(res) < np.median(res)+reject*np.median(np.abs(res)))[0]
-            popt,pcov = curve_fit(func_multi_poly,x[:,gd],y[gd],p0=pars,bounds=bounds)
-            res = y-func_multi_poly(x,*pars)
-            rms[row] = res[gd].std()
-            sig[row] = np.median(np.abs(res[gd]))
-            print(niter,row,len(gd),np.median(res),np.median(np.abs(res)),res[gd].std())
-            pars = popt
+            # use curve_fit to optimize paramtyers
+            try :
+                popt,pcov = curve_fit(func_multi_poly,x[:,gd],y[gd],p0=pars,bounds=bounds)
+                res = y-func_multi_poly(x,*pars)
+                rms[row] = res[gd].std()
+                sig[row] = np.median(np.abs(res[gd]))
+                print(niter,row,len(gd),np.median(res),np.median(np.abs(res)),res[gd].std())
+                pars = popt
+            except :
+                print('Solution failed for row: ', row)
+                popt = pars*0.
+        # plot individual line residuals if requested
         if plot :
             for ichip in range(3) :
                 plt = np.where(x[1,gd] == ichip+1)[0]
-                plots.plotp(ax[ichip],x[0,gd[plt]],y[gd[plt]]-func_multi_poly(x[:,gd[plt]],*popt),color=colors[irow%7],size=10)
+                z=np.zeros(len(plt))+row
+                plots.plotc(ax[ichip],x[0,gd[plt]],y[gd[plt]]-func_multi_poly(x[:,gd[plt]],*popt),z,zr=[0,300],
+                            xt='Pixel',yt='obs-fit wavelength',size=10)
         print(row,pars)
-        allpars[:,row] = pars
-    if plot : 
-        plots.plotp(ax2,np.arange(300),rms,color='r',size=10)
-        plots.plotp(ax2,np.arange(300),sig,color='g',size=10)
+        allpars[:,row] = popt
+    # plot rms/sig and chip locations if requested
+    if plot :
+        plots.plotp(ax2[0],np.arange(300),rms,color='r',size=10,xt='Row',yt='rms/sig')
+        plots.plotp(ax2[0],np.arange(300),sig,color='g',size=10)
+        for ichip in range(3) : 
+            for igroup in range(ngroup) :
+                y=allpars[npoly+igroup*3+ichip,:]
+                plots.plotp(ax2[1+ichip],np.arange(300),y,yr=[np.median(y)-2,np.median(y)+2],color=colors[ichip],
+                            size=10,yt='chip location')
+        fig.suptitle(name)
+        fig2.suptitle(name)
+        if hard is not None :
+            fig.savefig(hard+'.jpg')
+            fig2.savefig(hard+'_sig.jpg')
+
+    # output the apWavecal files with correct format
     x = np.zeros([3,2048])
     for ichip,chip in enumerate(chips) :
         hdu=fits.HDUList()
@@ -294,8 +337,8 @@ def wavecal(nums=[2420038],inst='apogee-n',rows=[150],npoly=4,plot=False,verbose
         hdu.append(fits.PrimaryHDU())
         hdu.append(fits.ImageHDU(chippars))
         hdu.append(fits.ImageHDU(chipwaves))
-        name='apWave-'+chip+'-{:08d}'.format(0)
-        hdu.writeto(name+'.fits',overwrite=True)
+        out='apPWave-'+chip+'-{:08d}'.format(name)
+        hdu.writeto(out+'.fits',overwrite=True)
 
 def visit(planfile,out=None,lco=False,waveid=True,skyfile='airglow_oct18a') :
     """ Determine positions of skylines for all frames in input planfile
@@ -461,80 +504,3 @@ def pix2wave(pix,wave0) :
     out[pix > 2047]= np.nan
     return out
 
-def mkpar(mjdstart,mjdend,out=None,lco=False) :
-    """ Make calibration file for wavecals between input dates
-    """
-    # open output file
-    print(mjdstart,mjdend,out)
-    if out is not None : f = open(out,'w')
-    indiv = []
-    sky = []
-    bad=np.loadtxt(os.environ['APOGEE_DIR']+'/data/cal/bad')
-    for mjd in range(mjdstart,mjdend) :
-        # get the files for this dat
-        if lco :
-            files = sorted(glob.glob(os.environ['APOGEE_DATA_2S']+'/'+str(mjd)+'/*-a-*.apz'))
-        else :
-            files = sorted(glob.glob(os.environ['APOGEE_DATA']+'/'+str(mjd)+'/*-a-*.apz'))
-        print(mjd)
-        dome=0
-        if len(files) > 3:
-            # look for sequences of QUARTZ, THARNE, UNE or THARNE, UNE, QUARTZ all at the same dither position
-            hdr1 = fits.open(files[0])[1].header
-            hdr2 = fits.open(files[1])[1].header
-            for ifile in range(2,len(files)-1) :
-                try :
-                    hdr3 = fits.open(files[ifile])[1].header
-                    #print(hdr1['DITHPIX'],hdr2['DITHPIX'],hdr3['DITHPIX'],hdr1['LAMPQRTZ'],hdr2['LAMPTHAR'],hdr3['LAMPUNE'])
-                    gd = False
-                    if hdr2['DITHPIX'] == hdr3['DITHPIX']  and hdr1['LAMPQRTZ'] and hdr2['LAMPTHAR'] and hdr3['LAMPUNE'] :
-                        thar = ifile-1
-                        une = ifile
-                        qrtz = ifile-2
-                        if len(np.where(bad == ifile-1)[0]) == 0 : gd = True
-                    elif hdr1['DITHPIX'] == hdr2['DITHPIX']  and hdr3['LAMPQRTZ'] and hdr1['LAMPTHAR'] and hdr2['LAMPUNE'] :
-                        thar = ifile-2
-                        une = ifile-1
-                        qrtz = ifile
-                        if len(np.where(bad == ifile-2)[0]) == 0 : gd = True
-                    if gd and out is not None : 
-                        f.write('wave 99999 99999 {:8s} {:8s},{:8s} {:8s}\n'.format(
-                             files[thar].split('-')[2].replace('.apz',''),
-                             files[thar].split('-')[2].replace('.apz',''),
-                             files[une].split('-')[2].replace('.apz',''),
-                             files[qrtz].split('-')[2].replace('.apz','')))
-                        indiv.append(files[thar].split('-')[2].replace('.apz','') )
-                    # look for sky frames and preceding domeflat, for LSF product (should be following domeflat for APO!)
-                    if hdr3['IMAGETYP'] == 'DomeFlat' : dome = ifile
-                    if hdr1['IMAGETYP'].strip() == 'Object' and hdr1['NFRAMES']>10 and hdr1['NFRAMES']<41 :
-                       sky.append(files[ifile-2].split('-')[2].replace('.apz','') )
-                       if out is not None : f.write('lsf 99999 99999 {:8s} {:8s} {:8s}\n'.format(
-                             files[ifile-2].split('-')[2].replace('.apz',''),
-                             files[ifile-2].split('-')[2].replace('.apz',''),
-                             files[dome].split('-')[2].replace('.apz','')))
-                except :
-                    pass
-                hdr1=hdr2
-                hdr2=hdr3
-    # now write the multiwave lines for groups of 10 individual wavecals
-    for i in range(0,len(indiv),10) :
-        name = indiv[i][0:4]+'0000'
-        if out is not None : f.write('multiwave 99999 99999 {:8s} {:8s}'.format(name,indiv[i]))
-        for j in range(1,10) : 
-            try:
-                f.write(',{:8s}'.format(indiv[i+j]))
-            except :
-                pass
-        f.write('\n')
-    f.close()
-
-def mkallpars(apo=True,lco=True) :
-    if lco :
-        mjds= [57600, 57966, 58335]
-        for i in range(len(mjds)-1) :
-            mkpar(mjds[i],mjds[i+1],out='lco-year{:1d}.par'.format(i+1),lco=True)
-    if apo :
-        mjds= [55800, 56130, 56512, 56876, 57230, 57600, 57966, 58335]
-        for i in range(len(mjds)-1) :
-            mkpar(mjds[i],mjds[i+1],out='apo-year{:1d}.par'.format(i+1))
-        

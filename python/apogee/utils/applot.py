e40b1e47352bbe2d04ad4200478e49d798e89b1f
Jon Holtzman <holtz@nmsu.edu>
2019-01-30 15:21:35 -0700

    refactor, allow pixel option

--- python/apogee/utils/applot.py
+++ python/apogee/utils/applot.py
@@ -1,12 +1,20 @@
+import numpy as np
 from tools import plots
+import pdb
 
-def chip(a,row=150,ax=None) :
+def chip(a,row=150,ax=None,pixel=False,visit=False,color=None) :
     """ Routine to plot 3 chips in 3 panels
     """
     if ax is None : fig,ax=plots.multi(1,3,hspace=0.3)
-    plots.plotl(ax[0],a['a'][4].data[row,:],a['a'][1].data[row,:])
-    plots.plotl(ax[1],a['b'][4].data[row,:],a['b'][1].data[row,:])
-    plots.plotl(ax[2],a['c'][4].data[row,:],a['c'][1].data[row,:])
+    if visit : rows=range(3)
+    else : rows=[row,row,row]
+    chips=['a','b','c']
+    x=np.arange(a['a'][1].header['NAXIS1'])
+    for ichip,chip in enumerate(chips) :
+        if pixel :
+            plots.plotl(ax[ichip],x,a[chip][1].data[rows[ichip],:],color=color)
+        else :
+            plots.plotl(ax[ichip],a[chip][4].data[rows[ichip],:],a[chip][1].data[rows[ichip],:],color=color)
 
     try : return fig,ax
     except : return

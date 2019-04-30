1dc9aa82266ef1ab279d8a28e93b9af66b69e51e
Jon Holtzman <holtz@nmsu.edu>
2019-04-30 12:50:04 -0600

    add new bits, but only to classes

--- python/apogee/utils/bitmask.py
+++ python/apogee/utils/bitmask.py
@@ -448,9 +448,10 @@ class ParamBitMask(BitMask):
     BitMask class for APOGEE ASPCAP bitmask (APOGEE_ASPCAPFLAG)
     '''
     name =['GRIDEDGE_BAD','CALRANGE_BAD','OTHER_BAD','FERRE_BAD','PARAM_MISMATCH_BAD','','','',
-          'GRIDEDGE_WARN','CALRANGE_WARN','OTHER_WARN','FERRE_WARN','PARAM_MISMATCH_WARN','','','',
-          'PARAM_FIXED','','','','','','','',
-          '','','','','','','','']
+           'GRIDEDGE_WARN','CALRANGE_WARN','OTHER_WARN','FERRE_WARN','PARAM_MISMATCH_WARN','OPTICAL_WARN','ERR_WARN','',
+           'PARAM_FIXED','','','','','','','',
+           'LOGG_CAL_RC','LOGG_CAL_RGB','LOGG_CAL_MS','LOGG_CAL_RGB_MS','','','','']
+
 
     level=[1,1,1,1,1,0,0,0,
              0,0,0,0,0,0,0,0,
@@ -469,10 +470,10 @@ class ParamBitMask(BitMask):
      'Parameter within 1/2 grid spacing of grid edge ',
      'Parameter in possibly unreliable range of calibration determination ',
      'Other warning condition ',
-     ' ',
+     'FERRE warning (not implemented?) ',
      'Elemental abundance from window differs significantly from parameter abundance ',
-     ' ',
-     ' ',
+     'Comparison with optical abundances suggests problem ',
+     'Large expected uncertainty or upper limit based on location in parameter space (Teff, [M/H], S/N) ',
      ' ',
      'Parameter set at fixed value, not fit',
      ' ',
@@ -482,9 +483,10 @@ class ParamBitMask(BitMask):
      ' ',
      ' ',
      ' ',
-     ' ',
-     ' ',
-     ' ',
+     'Use RC gravity calibration ',
+     'Use RGB gravity calibration ',
+     'Use MS gravity calibration ',
+     'Use RBG/MS transition gravity calibration ',
      ' ',
      ' ',
      ' ',

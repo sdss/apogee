d811003e317f81220204902c3a384d0679b24c4a
Jon Holtzman <holtz@nmsu.edu>
2020-05-18 15:58:21 -0600

    coment out logger for Python 3?

--- python/apogee/__init__.py
+++ python/apogee/__init__.py
@@ -10,7 +10,7 @@ import yaml
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(name).
-from .misc import log
+#from .misc import log
 
 
 def merge(user, default):

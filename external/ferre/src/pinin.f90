2dfefcbcf8656c1fbcaa3259af4b28178ff36ff4
Jon Holtzman <holtz@nmsu.edu>
2021-03-14 12:37:48 -0600

    modify pinin to modify other paramters on initialization if TIE is set

--- external/ferre/src/pinin.f90
+++ external/ferre/src/pinin.f90
@@ -8,9 +8,9 @@ subroutine    pinin(i,opti,w,pf,opf,obs,lambda_obs,e_obs,mobs,lsfarr,p)
 !
 
 use share, only: dp,ndim,nov,indv,indini,uu,   &
+                 ntie,indtie,typetie,ttie0,ttie,      &
                  nlambda1,mlsf,nlsf,           &
                  algor,init
-
 
 implicit none
 
@@ -20,7 +20,7 @@ integer, intent(in)   :: opti !0=regular run, 1=optimized on
 																														!and therefore a previous regular run
 																														!has taken place and the results are in p
 real(dp), intent(in)  :: w(nlambda1)   ! weights
-real(dp), intent(in)  :: pf(ndim)      ! vector of fixed parameters
+real(dp), intent(inout)  :: pf(ndim)      ! vector of fixed parameters
 real(dp), intent(in)  :: opf(ndim)     ! vector of fixed parameters
 																																				! before any previous optimization
 real(dp), intent(in)  :: obs(nlambda1)	! vector of observations
@@ -29,6 +29,8 @@ real(dp), intent(in)  :: e_obs(nlambda1)  ! uncertainties for observations
 real(dp), intent(in)  :: mobs             ! mean or median of obs array
 real(dp), intent(in)  :: lsfarr(mlsf,nlsf)    ! lsfarray
 real (dp), intent(inout) :: p(nov)
+real (dp)               :: pphys(ndim),pfphys(ndim)          !pp in physical units
+
 
 !locals
 integer   :: j,ii,kk   		
@@ -47,9 +49,34 @@ if (opti == 0) then !regular run
 			elseif (indini(j) == -1) then 
 				p(j)=pf(indv(j))
 			else
-          		p(j)=uu(j,i)/dble(indini(j)) + 1._dp/(2._dp*indini(j))
-        	endif   
+          	        	p(j)=uu(j,i)/dble(indini(j)) + 1._dp/(2._dp*indini(j))
+        	        endif   
+                        if (indtie(1) > 0) then
+                          pphys=pf
+                          pphys(indv(j)) = p(j)
+                          !write(*,*) pphys
+                          call physical(pphys)
+                          pfphys=pf
+                          call physical(pfphys)
+                          !write(*,*) pphys
+                          if (typetie == 0) then
+                            do ii=1,ntie
+                                  pphys(indtie(ii))=ttie0(ii)+sum(ttie(ii,1:ndim)*pphys(1:ndim))
+                            enddo
+                          else
+                            pphys(1:ndim)=pphys(1:ndim)-pfphys(1:ndim)
+                            do ii=1,ntie
+                                  pphys(indtie(ii))=ttie0(ii)+sum(ttie(ii,1:ndim)*pphys(1:ndim))
+                            enddo
+                            pphys(1:ndim)=pphys(1:ndim)+pfphys(1:ndim)
+                          endif
+                          write(*,*) pphys
+                          call normal(pphys)
+                          !write(*,*) pphys
+                          pf=pphys
+                        endif
 	  	enddo
+
 	  case default
 	    write(*,*)'init has an illegal value'
 	    stop 
